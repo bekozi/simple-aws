70e5d60835e3808fbbb9875589df812ce488bf2b
Ben Koziol <benkoziol@gmail.com>
2014-05-13 16:19:30 -0600

    added src directory with module and test files

---
+++ src/aws.py
@@ -0,0 +1,94 @@
+from ConfigParser import SafeConfigParser
+import os
+import boto.ec2
+from boto.ec2.autoscale import LaunchConfiguration
+import datetime
+import time
+
+
+class AwsManager(object):
+
+    def __init__(self, conf_path, filtered=True, only_running=True):
+        self.conf_path = conf_path
+        self.filtered = filtered
+        self.only_running = only_running
+        self._conn = None
+        self._parser = SafeConfigParser()
+        self._parser.read(self.conf_path)
+        self._cfg = dict(self._parser.items('aws'))
+
+    @property
+    def conn(self):
+        if self._conn is None:
+            self._conn = self._get_aws_connection_()
+        return(self._conn)
+
+    def get_instances(self):
+        reservations = self.conn.get_all_reservations()
+        instances = [i for r in reservations for i in r.instances]
+        instances = {i.id: i for i in instances}
+        if self.only_running:
+            instances = {i.id: i for i in instances.values() if i.update() == 'running'}
+        if self.filtered:
+            instances = {i.id: i for i in instances.values() if self._filter_(i)}
+        return(instances)
+
+    def get_instance_by_name(self, name):
+        ret = None
+        for instance in self.get_instances().itervalues():
+            if instance.tags['Name'] == name:
+                ret = instance
+        if ret is None:
+            raise(ValueError('Name not found.'))
+        else:
+            return(ret)
+
+    def launch_new_instance(self, name, wait=True):
+        image_id = self._cfg['image_id']
+        security_group = self._cfg['security_group']
+        instance_type = self._cfg['instance_type']
+        key_name = self._cfg['key_name']
+
+        if not self._get_is_unique_tag_(name, 'Name'):
+            msg = 'The assigned instance name "{0}" is not unique.'.format(name)
+            raise(ValueError(msg))
+
+        reservation = self.conn.run_instances(image_id, key_name=key_name, instance_type=instance_type, security_groups=[security_group])
+        instance = reservation.instances[0]
+        self.conn.create_tags([instance.id], {"Name": name})
+
+        if wait:
+            status = instance.update()
+            while status != 'running':
+                time.sleep(1)
+                status = instance.update()
+
+        return(instance)
+
+    def _filter_(self, instance):
+        if instance.key_name == self._cfg['key_name']:
+            ret = True
+        else:
+            ret = False
+        return(ret)
+
+    def _get_aws_connection_(self):
+        region = self._cfg['region']
+
+        aws_access_key_id = self._cfg['access_key_id']
+        aws_secret_access_key = self._cfg['secret_access_key']
+
+        conn = boto.ec2.connect_to_region(region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+        if conn is None:
+            raise(RuntimeError('Unable to launch instance.'))
+
+        return(conn)
+
+    def _get_is_unique_tag_(self, tag_value, tag_key):
+        instances = self.get_instances()
+        ret = True
+        for i in instances.itervalues():
+            if i.tags.get(tag_key) == tag_value:
+                ret = False
+        return(ret)
---
+++ src/test_aws.py
@@ -0,0 +1,78 @@
+import unittest
+import itertools
+from aws import AwsManager
+import os
+
+
+CONF_PATH = os.path.join(os.path.expanduser('~/.config/ocgis.cfg'))
+
+
+class TestAwsManager(unittest.TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        m = AwsManager(CONF_PATH)
+        assert(m.get_instances() == {})
+
+    def test_constructor(self):
+        filtered = [True, False]
+        only_running = [True, False]
+        conf_path = [None, CONF_PATH]
+        for f, a, c in itertools.product(filtered, only_running, conf_path):
+            try:
+                m = AwsManager(c, filtered=f, only_running=a)
+                self.assertIsInstance(m._cfg, dict)
+            except TypeError:
+                if c is None:
+                    pass
+                else:
+                    raise
+
+    def test_get_instance_by_name(self):
+        m = AwsManager(CONF_PATH)
+        i1 = m.launch_new_instance('_foo_name_test', wait=True)
+        try:
+            instance = m.get_instance_by_name('_foo_name_test')
+            self.assertEqual(instance.tags['Name'], '_foo_name_test')
+        finally:
+            i1.terminate()
+
+    def test_launch_instance_with_wait(self):
+        m = AwsManager(CONF_PATH)
+        # print('launching 1')
+        i1 = m.launch_new_instance('_foo_test_1')
+        self.assertEqual(i1.tags['Name'], '_foo_test_1')
+        # print('launching 2')
+        i2 = m.launch_new_instance('_foo_test_2')
+        # print('instances launched')
+        try:
+            instances = m.get_instances()
+            self.assertEqual(len(instances), 2)
+            self.assertEqual(set([i.update() for i in instances.values()]), set(['running']))
+        finally:
+            i1.terminate()
+            i2.terminate()
+        self.assertEqual(m.get_instances(), {})
+
+    def test_launch_instance_without_wait(self):
+        m = AwsManager(CONF_PATH)
+        i1 = m.launch_new_instance('_foo_test_1', wait=False)
+        try:
+            self.assertNotEqual(i1.update(), 'running')
+        finally:
+            i1.terminate()
+        self.assertEqual(m.get_instances(), {})
+
+    def test_name_must_be_unique(self):
+        m = AwsManager(CONF_PATH)
+        self.assertEqual(m.get_instances(), {})
+        try:
+            i1 = m.launch_new_instance('_foo_test_4', wait=True)
+            with self.assertRaises(ValueError):
+                i2 = m.launch_new_instance('_foo_test_4', wait=True)
+        finally:
+            try:
+                i1.terminate()
+                i2.terminate()
+            except UnboundLocalError:
+                pass
